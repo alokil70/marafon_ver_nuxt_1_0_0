34a21ecce868e56833ec27ef47a5a44b9574c940
Alexandr <Alokil70@gmail.com>
2020-05-02 21:29:50 +0300

    add editors

--- layouts/manager.vue
+++ layouts/manager.vue
@@ -4,7 +4,9 @@
             v-model="drawer"
             :mini-variant="miniVariant"
             :clipped="clipped"
+            mobile-break-point="1120"
             fixed
+            width="200"
             app
         >
             <v-list>
@@ -26,19 +28,6 @@
         </v-navigation-drawer>
         <v-app-bar :clipped-left="clipped" fixed app>
             <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-            <v-btn icon @click.stop="miniVariant = !miniVariant">
-                <v-icon
-                    >mdi-{{
-                        `chevron-${miniVariant ? 'right' : 'left'}`
-                    }}</v-icon
-                >
-            </v-btn>
-            <v-btn icon @click.stop="clipped = !clipped">
-                <v-icon>mdi-application</v-icon>
-            </v-btn>
-            <v-btn icon @click.stop="fixed = !fixed">
-                <v-icon>mdi-minus</v-icon>
-            </v-btn>
             <v-toolbar-title v-text="title" />
             <v-spacer />
             <v-btn @click.stop="rightDrawer = !rightDrawer">
@@ -80,12 +69,12 @@ export default {
     data() {
         return {
             clipped: true,
-            drawer: false,
+            drawer: true,
             fixed: false,
             items: [
                 {
                     icon: 'mdi-apps',
-                    title: 'Welcome',
+                    title: 'Главная',
                     to: '/',
                 },
                 {
